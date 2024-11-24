cc007c25bcb66bc8bc67add15a20ec74b1cbff11
ariel <ariel.leira0910@gmail.com>
2024-11-25 00:53:13 +0800

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Student",
+                DateOfBirth = new DateTime(1992, 09, 10)
+            };
+            return View(data);
+        }
+    }
+}
---
+++ LeaveManagementSystem.Web/Models/TestViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+
+namespace LeaveManagementSystem.Web.Models
+{
+    public class TestViewModel
+    {
+        public string Name { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
